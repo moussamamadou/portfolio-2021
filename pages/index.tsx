f14300aa072dc98731b6c270899fcb22a9698de3
Moussa Mamadou <87030270+moussamamadou@users.noreply.github.com>
2021-09-27 04:12:52 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,37 @@
-export default function Home(): JSX.Element {
+import Seo from "../components/Seo";
+import Nav from "../components/Nav";
+import About from "../components/About";
+import Contact from "../components/Contact";
+import Hero from "../components/Hero";
+import Work from "../components/Work";
+import Footer from "../components/Footer";
+import data from "../frontaid/data.json";
+
+export default function Home({ data, locale }: any): JSX.Element {
   return (
-    <div className="">
-      <h1>Next - TypeScript - Tailwind</h1>
-    </div>
+    <>
+      <Seo data={data.seo} />
+      <header className="container">
+        <Nav data={data.nav} />
+      </header>
+      <main className="container">
+        <Hero data={data.hero} />
+        <Work data={data.work} />
+        <About data={data.about} />
+        <Contact data={data.contact} />
+      </main>
+      <footer className="container">
+        <Footer />
+      </footer>
+    </>
   );
+}
+
+export async function getStaticProps({ locale }: any) {
+  return {
+    props: {
+      data: data[locale],
+      locale: locale,
+    },
+  };
 }
