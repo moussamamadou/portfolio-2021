ad2c978e7b900aa3ee07ea062225837c45309812
Moussa Mamadou <87030270+moussamamadou@users.noreply.github.com>
2021-09-29 10:53:18 +0200

    Correction Splitext for Vercel

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,41 +1,44 @@
-import parser from "html-react-parser";
-import { gsap } from "gsap";
-import SplitText from "../utils/Split3.min";
-import { useEffect, useRef } from "react";
+import Reveal from "react-awesome-reveal";
+import { keyframes } from "@emotion/react";
+const customAnimation = keyframes`
+  from {
+    opacity: 0;
+    transform: translate3d(0, 1rem, 0);
+  }
 
+  to {
+    opacity: 1;
+    transform: translate3d(0, 0, 0);
+  }
+`;
 const Hero = ({ data }: any) => {
-  const ref = useRef(null);
-  useEffect(() => {
-    if (typeof window !== undefined) {
-      var split = new SplitText(ref.current, {
-        type: "words",
-      });
-      gsap.from(split.words, {
-        duration: 1.2,
-        delay: 0.1,
-        y: "3rem",
-        opacity: 0,
-        stagger: 0.01,
-        ease: "power3",
-      });
-    }
-  }, []);
   return (
     <section
       data-scroll-section
       className=" relative mt-4 min-h-[80vh] w-full flex flex-col justify-center"
     >
-      <h1
-        ref={ref}
-        data-scroll
-        data-scroll-direction="vertical"
-        data-scroll-speed="-1"
-        data-scroll-class="appear"
-        className="px-0 xl:px-20 w-full"
-        id="hero"
-      >
-        {parser(data.text)}
-      </h1>
+      <Reveal keyframes={customAnimation} cascade delay={0.5} damping={0.2}>
+        <h1
+          data-scroll
+          data-scroll-direction="vertical"
+          data-scroll-speed="-1"
+          data-scroll-class="appear"
+          className="px-0 xl:px-20 py-4 w-full font-bold"
+          id="hero"
+        >
+          {data.textFirst}
+        </h1>
+        <h1
+          data-scroll
+          data-scroll-direction="vertical"
+          data-scroll-speed="-1"
+          data-scroll-class="appear"
+          className="px-0 xl:px-20 py-4 w-full  tracking-wider"
+          id="hero"
+        >
+          {data.textSecond}
+        </h1>
+      </Reveal>
     </section>
   );
 };
